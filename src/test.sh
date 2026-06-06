e4720120138ce39d6fc79fadc1aa183a505e5d2e
AhmedKarimo <ineutx@gmail.com>
2026-06-06 19:59:48 +0300

    Creates a new NodeJS app

---
+++ src/test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+output=$(node app.js)
+
+expected="Hello, Ahmed!"
+
+if [ "$output" = "$expected" ]; then
+    echo "✅ Test Passed"
+    exit 0
+else
+    echo "❌ Test Failed"
+    echo "Expected: $expected"
+    echo "Got: $output"
+    exit 1
+fi
